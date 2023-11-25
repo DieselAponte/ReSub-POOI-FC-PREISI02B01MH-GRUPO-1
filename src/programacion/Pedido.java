9058108adc631456cb9f2be737e2d331a9ad9408
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-11-25 11:59:09 -0500

    Subiento Pedido.java

--- src/programacion/Pedido.java
+++ src/programacion/Pedido.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package programacion;
 
-/**
- *
- * @author NnShzz
- */
 public class Pedido {
-    
+    private int idPedido;
+    private String descripcionDelPedido;
+    private String estado;
+
+    public Pedido(int idPedido, String descripcionDelPedido, String estado) {
+        this.idPedido = idPedido;
+        this.descripcionDelPedido = descripcionDelPedido;
+        this.estado = estado;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getDescripcionDelPedido() {
+        return descripcionDelPedido;
+    }
+
+    public void setDescripcionDelPedido(String descripcionDelPedido) {
+        this.descripcionDelPedido = descripcionDelPedido;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void cambiarEstado(String estado) {
+           this.estado = estado;
+    }
 }
